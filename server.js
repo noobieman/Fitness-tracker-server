7a6350dc99d94ef7e84ad3a7db5ff1f9cce58581
Abhijith <abijithrakan300@gmail.com>
2025-03-03 14:31:17 +0530

    Fixed

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // Ensure JSON parsing
 // Middleware
-
+app.use(express.json());
 
 const allowedOrigins = [
   "http://localhost:3000", // For local testing
